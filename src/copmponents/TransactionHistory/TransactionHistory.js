b47ddcb9988cad31b2b68606dbebf923615f221f
Tanya Sabadyshyna <sabadyshyna@gmail.com>
2020-12-01 01:32:31 +0200

    add TransactionsHistory section

--- src/copmponents/TransactionHistory/TransactionHistory.js
+++ src/copmponents/TransactionHistory/TransactionHistory.js
@@ -12,16 +12,25 @@ const Transaction = ({type, amount, currency}) => {
 
 function TransactionsHistory({ items }) {
   return (
-    <tbody>
+<table className="transaction-history">
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>    
+      <tbody>
       {items.map(item => ( 
-        <Transaction
-            key={item.id}
+          <Transaction
+            key={item.id} 
             type={item.type}
             amount={item.amount}
             currency={item.currency}
         />
       ))}
-    </tbody>
+      </tbody>
+</table>
   );
 }
 
